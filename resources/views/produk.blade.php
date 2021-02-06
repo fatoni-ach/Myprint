cbb9eabddeba0810127a7f18a82b9d00e794adcb
fatoni-ach <fatoni.noah@gmail.com>
2021-02-07 01:48:47 +0700

    setting heroku commit

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -36,7 +36,7 @@
       <span class="sr-only">Next</span>
     </a>
   @else
-    <ol class="carousel-indicators">
+    <!-- <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
@@ -59,7 +59,8 @@
     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
-    </a>
+    </a> -->
+    <br>
   @endif
   
 </div>
